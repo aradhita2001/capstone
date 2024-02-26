4458f8f0b45c777a1d322ffa468a3812bd7d610c
username <wecp-dummy@gmail.com>
2024-02-26 13:06:18 +0000

    Added Required Custom Exceptions All Exceptions extend Runtime Exception 	new file:   server/wecp-spring-boot-jwt-app/src/main/java/com/wecp/exception/AccountNotFoundException.java 	new file:   server/wecp-spring-boot-jwt-app/src/main/java/com/wecp/exception/AuthenticationFailureException.java 	new file:   server/wecp-spring-boot-jwt-app/src/main/java/com/wecp/exception/OutOfBalanceException.java 	new file:   server/wecp-spring-boot-jwt-app/src/main/java/com/wecp/exception/UserNameNotFoundException.java

---
+++ server/wecp-spring-boot-jwt-app/src/main/java/com/wecp/exception/AccountNotFoundException.java
@@ -0,0 +1,23 @@
+package com.wecp.exception;
+
+public class AccountNotFoundException extends RuntimeException {
+
+    public AccountNotFoundException() {
+    }
+
+    public AccountNotFoundException(String arg0) {
+        super(arg0);
+    }
+
+    public AccountNotFoundException(Throwable arg0) {
+        super(arg0);
+    }
+
+    public AccountNotFoundException(String arg0, Throwable arg1) {
+        super(arg0, arg1);
+    }
+
+    public AccountNotFoundException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+        super(arg0, arg1, arg2, arg3);
+    }
+}
